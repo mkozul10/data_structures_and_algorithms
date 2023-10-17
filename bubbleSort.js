9a56de606e71fe0864f05e900e66da2d526eb2d5
Mario <mariokozul24@gmail.com>
2023-10-17 13:11:03 +0200

    bubble sort

---
+++ bubbleSort.js
@@ -0,0 +1,16 @@
+const arr = [2,8,5,3,9,4,1];
+
+const sort = (arr) => {
+    for(let i = 0; i < arr.length - 1; i++){
+        for(let j = 0; j < arr.length - 1 - i; j++){
+            if(arr[j + 1] < arr[j]){
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(sort(arr));
